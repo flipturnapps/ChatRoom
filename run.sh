8a35fe243901378e11d8d1a9774430b079ed5183
Kevin Kellar <kellar.kevin@gmail.com>
2018-01-28 11:40:08 -0800

    made the project executable and fixed start frames bounds

---
+++ run.sh
@@ -0,0 +1,2 @@
+javac -d bin/ -cp src:.:lib/* src/com/flipturnapps/chatroom/*/*.java
+java -cp bin:.:lib/* com.flipturnapps.chatroom.net.Start
--- src/com/flipturnapps/chatroom/gui/StartFrame.java
+++ src/com/flipturnapps/chatroom/gui/StartFrame.java
@@ -62,7 +62,7 @@ public class StartFrame extends JFrame {
 		//Application.getApplication().setDockIconImage(ResourceGetter.getBlueIcon()); //for mac
 		setTitle("ChatRoom Starter (v " + versionId + ")");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 480, 300);
+		setBounds(100, 100, 550, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
